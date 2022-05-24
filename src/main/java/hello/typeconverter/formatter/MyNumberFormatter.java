62e7b44d629c370da1daaae1aa2f7741714aeeec
username <wogh0013@naver.com>
2022-05-24 23:35:39 +0900

    converter last commit

--- src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
+++ src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -1,8 +1,27 @@
 package hello.typeconverter.formatter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.Formatter;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 @Slf4j
-public class MyNumberFormatter {
+public class MyNumberFormatter implements Formatter<Number>{
 
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        log.info("text={}, locale={}", text, locale);
+        //"1,000" -> 1000
+        //↓ 자바에서 제공
+        NumberFormat format = NumberFormat.getInstance(locale);
+        return format.parse(text); //숫자로 반환
+    }
+
+    @Override
+    public String print(Number object, Locale locale) {
+        log.info("object={}, locale={}", object, locale);
+        return NumberFormat.getInstance(locale).format(object); //String으로 반환 
+    }
 }
